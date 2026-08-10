fffa34a3d924d3ac03a4000aa4d7f9f2747231f6
Mordy Pesach <mordypassover@gmail.com>
2026-08-10 10:43:01 +0300

    created first migration

---
+++ OfficeManagementSystemApi/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace OfficeManagementSystemApi.Data;
+
+public class MyDbContext: DbContext
+{
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+
+}
--- OfficeManagementSystemApi/Program.cs
+++ OfficeManagementSystemApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using OfficeManagementSystemApi.Data;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string"
+        + "'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
